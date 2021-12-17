31d5301c96c66cb60f5122560be4826c5e4edee0
Bhon Bunnag <bhonris@gmail.com>
2021-12-17 21:02:25 +0700

    asdf

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme'
+import toJSON from "enzyme-to-json";
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const component = shallow(<App />);
+  expect(toJSON(component)).toMatchSnapshot();
 });
